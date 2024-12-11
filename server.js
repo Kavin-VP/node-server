3e4d90dc3b6d7bced7c5791320fa8d152197d7f4
Kavin V P <64819221+Kavin-VP@users.noreply.github.com>
2024-12-11 09:20:35 +0530

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const webPush = require('web-push');
 const app = express();
-const port = 3000;
+const port = 80;
 const cors = require('cors');
 
 app.use(cors());
